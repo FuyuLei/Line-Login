dff574a18f60f1b040545d6e8fa64add884a87b4
108406 <raralei8860@gmail.com>
2019-09-25 14:49:58 +0800

    myApp

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.get('/', (req, res) => {
   if (req.session.authPass) {
     const profile = req.session.profile;
     res.render('success', profile); //自訂成功登入頁面
-    console.log('===============' + profile[0]);
+    console.log('===============' + profile.userId);
   } else if (req.session.errMsg) {
     res.render('login', {  //自訂尚未登入頁面，顯示錯誤訊息
       ErrMsg: req.session.errMsg
